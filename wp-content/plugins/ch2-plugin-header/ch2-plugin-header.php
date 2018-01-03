303ea1ea69a36a79081b79f76297cbe009d8bb95
Manu Gonzalez <jgonzalezcantarero@gmail.com>
2018-01-04 00:32:02 +0100

    Creating a plugin file and header

---
+++ wp-content/plugins/ch2-plugin-header/ch2-plugin-header.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Chapter 2 - Plugin Header
+Plugin URI:
+Description: Declares a plugin that will be visible in the WordPress admin interface
+Version: 1.0
+Author: Manu
+Author URI:
+License: GPLv2
+ */
