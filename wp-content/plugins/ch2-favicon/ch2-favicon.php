6eb35e3beed3efc1f0505fb35a461518c27da34a
Manu Gonzalez <jgonzalezcantarero@gmail.com>
2018-01-04 01:03:36 +0100

    Using WordPress path utility functions to load external files and images

---
+++ wp-content/plugins/ch2-favicon/ch2-favicon.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Chapter 2 - Favicon
+Plugin URI:
+Description:
+Version: 1.0
+Author: Manu
+Author URI:
+License: GPLv2
+ */
+add_action( 'wp_head', 'ch2fi_page_header_output' );
+
+function ch2fi_page_header_output() {
+	$site_icon_url = get_site_icon_url();
+	if ( ! empty( $site_icon_url ) ) {
+		wp_site_icon();
+	} else {
+		$icon_url = plugins_url( 'favicon.ico', __FILE__ );
+		?>
+
+        <link rel="shortcut icon" href="<?php echo $icon_url; ?>"/>
+	<?php }
+}
